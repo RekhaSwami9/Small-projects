4c03e9402963bc0b6f9b55821ecc17635d76cb00
RekhaSwami9 <157201097+RekhaSwami9@users.noreply.github.com>
2024-05-20 10:48:26 +0530

    Add files via upload

---
+++ dark nd light mode/app.js
@@ -0,0 +1,12 @@
+
+const btn = document.querySelector('.light-dark');
+btn.addEventListener('click', ()=>{
+    if(!document.body.classList.contains('dark'))
+    {
+        document.body.classList.add('dark');
+        btn.textContent = 'Light'
+    } else{
+        document.body.classList.remove('dark');
+        btn.textContent = 'Dark'
+    }
+});
