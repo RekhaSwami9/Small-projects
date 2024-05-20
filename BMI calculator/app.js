ccf459fd05cc91887caa437f3b1271f956b3e543
RekhaSwami9 <157201097+RekhaSwami9@users.noreply.github.com>
2024-05-20 11:07:55 +0530

    Add files via upload

---
+++ BMI calculator/app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#bmiForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+
+  if (isNaN(height) || height <= 0) {
+    results.innerHTML = `Please enter a valid height`;
+  } else if (isNaN(weight) || weight <= 0) {
+    results.innerHTML = `Please enter a valid weight`;
+  } else {
+    const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `<span>${BMI}</span>`;
+  }
+});
